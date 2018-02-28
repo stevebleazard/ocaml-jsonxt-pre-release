cba92f9724c19a490f1bd29e2f7edd9d064ddb5a
Steve Bleazard <stevebleazard@googlemail.com>
2018-02-28 13:31:19 +0000

    added stream module

--- src/json.mli
+++ src/json.mli
@@ -46,3 +46,30 @@ module Strict : sig
       ]
   type t = json
 end
+
+module Stream : sig
+  type json =
+      [
+      | `Null
+      | `Bool of bool
+      | `Int of int
+      | `Intlit of string
+      | `Float of float
+      | `Floatlit of string
+      | `String of string
+      | `Stringlit of string
+      | `Tuple of json list
+      | `Variant of (string * json option)
+      | `As
+      | `Ae
+      | `Os
+      | `Oe
+      | `Name of string
+      | `Value of json
+      | `Infinity
+      | `Neg_infinity
+      | `Nan
+      ]
+  type t = json
+end
+
