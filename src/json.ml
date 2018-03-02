04963da23c3bfcd914a948888a32ee5539cb5cc3
Steve Bleazard <stevebleazard@googlemail.com>
2018-03-02 12:53:51 +0000

    added json module containing exported types

---
+++ src/json.ml
@@ -0,0 +1,86 @@
+open Tokens
+
+type json =
+    [
+    | `Null
+    | `Bool of bool
+    | `Int of int
+    | `Intlit of string
+    | `Float of float
+    | `Floatlit of string
+    | `String of string
+    | `Stringlit of string
+    | `Assoc of (string * json) list
+    | `List of json list
+    | `Tuple of json list
+    | `Variant of (string * json option)
+    ]
+    constraint json = Json_parse_type.json
+
+type t = json
+
+module Extended = struct
+  type nonrec json = json
+  type t = json
+end
+
+module Yojson = struct
+  type nonrec json = json
+  type t = json
+end
+
+module Basic = struct
+  type json =
+      [
+      | `Null
+      | `Bool of bool
+      | `Int of int
+      | `Float of float
+      | `String of string
+      | `Assoc of (string * json) list
+      | `List of json list
+      ]
+      constraint json = Json_parse_type.Basic.json
+  type t = json
+end
+
+module Strict = struct
+  type json =
+      [
+      | `Null
+      | `Bool of bool
+      | `Float of float
+      | `String of string
+      | `Assoc of (string * json) list
+      | `List of json list
+      ]
+      constraint json = Json_parse_type.Strict.json
+  type t = json
+end
+
+module Stream = struct
+  type json =
+      [
+      | `Null
+      | `Bool of bool
+      | `Int of int
+      | `Intlit of string
+      | `Float of float
+      | `Floatlit of string
+      | `String of string
+      | `Stringlit of string
+      | `Tuple of json list
+      | `Variant of (string * json option)
+      | `As
+      | `Ae
+      | `Os
+      | `Oe
+      | `Name of string
+      | `Value of json
+      | `Infinity
+      | `Neg_infinity
+      | `Nan
+      ]
+      constraint json = Json_parse_type.Stream.json
+  type t = json
+end
