84348e1fd1899472eb9e5de44f4ebfbd173040f1
Steve Bleazard <stevebleazard@googlemail.com>
2018-03-09 10:20:38 +0000

    added json_of_file functions to json_basic mli

--- src/json_basic.mli
+++ src/json_basic.mli
@@ -3,4 +3,6 @@ type t = json
 
 val json_of_string : ?strict:bool -> string -> (json, string) result
 val json_of_string_exn : ?strict:bool -> string -> json
+val json_of_file : ?strict:bool -> string -> (json, string) result
+val json_of_file_exn : ?strict:bool -> string -> json
 val of_string : string -> json
