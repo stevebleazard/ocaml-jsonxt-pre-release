a33ee7559d35a6c8e13a1d61cfe2b6b075a55589
stevebleazard <stevebleazard@googlemail.com>
2018-03-15 20:15:12 +0000

    added to_string / json_to_string to basic

--- src/basic.ml
+++ src/basic.ml
@@ -2,3 +2,6 @@ module Lexxer = Compliant_lexxer.Make(Json_parse_types.Basic)
 module Parser = Parser.Make(Json_parse_types.Basic)
 
 include Json_string_file.Make (Lexxer) (Parser)
+
+let json_to_string = Json_writer.to_string
+let to_string = Json_writer.to_string
--- src/basic.mli
+++ src/basic.mli
@@ -5,4 +5,8 @@ val json_of_string : ?strict:bool -> string -> (json, string) result
 val json_of_string_exn : ?strict:bool -> string -> json
 val json_of_file : ?strict:bool -> string -> (json, string) result
 val json_of_file_exn : ?strict:bool -> string -> json
+val json_of_string : ?strict:bool -> string -> (json, string) result
 val of_string : string -> json
+
+val json_to_string : json -> string
+val to_string : json -> string
