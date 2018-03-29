1f08cd033749516be986c6c1adc1bc0296048285
stevebleazard <stevebleazard@googlemail.com>
2018-03-29 18:38:04 +0100

    cleanup function val defs

--- src/json_writer_file.mli
+++ src/json_writer_file.mli
@@ -1,35 +1,21 @@
 module type Intf = sig
-  val json_to_file
-   :  string
-   -> ([< `Assoc of (string * 'a) list
-       | `Bool of bool
-       | `Float of float
-       | `Floatlit of string
-       | `Int of int
-       | `Intlit of string
-       | `List of 'a list
-       | `Null
-       | `String of string
-       | `Stringlit of string
-       | `Tuple of 'a list
-       | `Variant of string * 'a option ] as 'a)
-   -> unit
+  type 'a json_all =
+    [< `Assoc of (string * 'a) list
+    | `Bool of bool
+    | `Float of float
+    | `Floatlit of string
+    | `Int of int
+    | `Intlit of string
+    | `List of 'a list
+    | `Null
+    | `String of string
+    | `Stringlit of string
+    | `Tuple of 'a list
+    | `Variant of string * 'a option ] as 'a
 
-  val json_to_channel
-   :  out_channel
-   -> ([< `Assoc of (string * 'a) list
-       | `Bool of bool
-       | `Float of float
-       | `Floatlit of string
-       | `Int of int
-       | `Intlit of string
-       | `List of 'a list
-       | `Null
-       | `String of string
-       | `Stringlit of string
-       | `Tuple of 'a list
-       | `Variant of string * 'a option ] as 'a)
-   -> unit
+  val json_to_file : string -> 'a json_all -> unit
+  val json_to_channel :  out_channel ->  'a json_all -> unit
+
 end
 
 module Make (Compliance : Compliance.S) : Intf
