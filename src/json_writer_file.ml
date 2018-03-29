4c8c2a98a700a8ecc9c61756e5bf01d442d73893
stevebleazard <stevebleazard@googlemail.com>
2018-03-29 18:44:10 +0100

    refactor to move new constrained json type to new file

--- src/json_writer_file.ml
+++ src/json_writer_file.ml
@@ -1,36 +1,9 @@
 module type Intf = sig
-  type 'a json_all =
-    [< `Assoc of (string * 'a) list
-    | `Bool of bool
-    | `Float of float
-    | `Floatlit of string
-    | `Int of int
-    | `Intlit of string
-    | `List of 'a list
-    | `Null
-    | `String of string
-    | `Stringlit of string
-    | `Tuple of 'a list
-    | `Variant of string * 'a option ] as 'a
-
-  val json_to_file : string -> 'a json_all -> unit
-  val json_to_channel :  out_channel ->  'a json_all -> unit
+  val json_to_file : string -> 'a Json_internal.constained -> unit
+  val json_to_channel :  out_channel -> 'a Json_internal.constained -> unit
 end
 
 module Make (Compliance : Compliance.S) : Intf = struct
-  type 'a json_all =
-    [< `Assoc of (string * 'a) list
-    | `Bool of bool
-    | `Float of float
-    | `Floatlit of string
-    | `Int of int
-    | `Intlit of string
-    | `List of 'a list
-    | `Null
-    | `String of string
-    | `Stringlit of string
-    | `Tuple of 'a list
-    | `Variant of string * 'a option ] as 'a
 
   open Printf
 
