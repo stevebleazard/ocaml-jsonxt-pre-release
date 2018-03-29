4d7df2527d479fecb99412e752788ed4ab434aed
stevebleazard <stevebleazard@googlemail.com>
2018-03-29 18:47:41 +0100

    move to single contrained type

--- src/json_writer_string.ml
+++ src/json_writer_string.ml
@@ -1,48 +1,7 @@
 module type Intf = sig
-  val json_to_string
-   : ([< `Assoc of (string * 'a) list
-      | `Bool of bool
-      | `Float of float
-      | `Floatlit of string
-      | `Int of int
-      | `Intlit of string
-      | `List of 'a list
-      | `Null
-      | `String of string
-      | `Stringlit of string
-      | `Tuple of 'a list
-      | `Variant of string * 'a option ] as 'a)
-   -> (string, string) result
-
-  val json_to_string_exn
-   : ([< `Assoc of (string * 'a) list
-      | `Bool of bool
-      | `Float of float
-      | `Floatlit of string
-      | `Int of int
-      | `Intlit of string
-      | `List of 'a list
-      | `Null
-      | `String of string
-      | `Stringlit of string
-      | `Tuple of 'a list
-      | `Variant of string * 'a option ] as 'a)
-   -> string
-
-  val to_string
-   : ([< `Assoc of (string * 'a) list
-      | `Bool of bool
-      | `Float of float
-      | `Floatlit of string
-      | `Int of int
-      | `Intlit of string
-      | `List of 'a list
-      | `Null
-      | `String of string
-      | `Stringlit of string
-      | `Tuple of 'a list
-      | `Variant of string * 'a option ] as 'a)
-   -> string
+  val json_to_string : 'a Json_internal.constained -> (string, string) result
+  val json_to_string_exn : 'a Json_internal.constained -> string
+  val to_string : 'a Json_internal.constained -> string
 end
 
 module Make (Compliance : Compliance.S) : Intf = struct
