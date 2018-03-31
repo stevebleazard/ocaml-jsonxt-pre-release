f6420e71a48ddc5caea9fc2fe722788ad5e6fd5f
stevebleazard <stevebleazard@googlemail.com>
2018-03-31 12:34:35 +0100

    yojson refactored to support basic and safe

--- src/yojson.ml
+++ src/yojson.ml
@@ -1,45 +1,95 @@
-module Compliance = struct
-  type json = Json.json
-  type t = json
+module Basic = struct
+  module Compliance = struct
+    type json = Json.Basic.json
+    type t = json
 
-  open Tokens
+    open Tokens
 
-  let lex_number token = token
-  let lex_integer token = token (* CR sbleazard: fix bounds *)
-  let lex_largeint _ = COMPLIANCE_ERROR "Integer out of bounds"
+    let lex_number token = token
+    let lex_integer token = token
+    let lex_largeint _ = COMPLIANCE_ERROR "Integer out of bounds"
 
-  let lex_variant _ = true
-  let lex_tuple _ = true
+    let lex_variant _ = false
+    let lex_tuple _ = false
 
-  let number_to_string f =
-    match classify_float f with
-    | FP_normal | FP_subnormal | FP_zero ->
-      Json_float.string_of_float_fast_int f
-    | FP_infinite ->
-      if f < 0. then "-Infinity" else "Infinity"
-    | FP_nan ->
-      "NaN"
+    let number_to_string f =
+      match classify_float f with
+      | FP_normal | FP_subnormal | FP_zero ->
+        Json_float.string_of_float_fast_int f
+      | FP_infinite ->
+        if f < 0. then "-Infinity" else "Infinity"
+      | FP_nan ->
+        "NaN"
 
-  let integer i = `Int i
-  let null = `Null
-  let string s = `String s
-  let bool b = `Bool b
-  let assoc a = `Assoc a
-  let list l = `List l
-  let tuple l = `Tuple l
-  let variant k v = `Variant (k, v)
+    let integer i = `Int i
+    let null = `Null
+    let string s = `String s
+    let bool b = `Bool b
+    let assoc a = `Assoc a
+    let list l = `List l
+    let tuple l = raise (Failure "tuples not supported in yojson basic mode")
+    let variant l = raise (Failure "variants not supported in yojson basic mode")
 
-  let number = function
-  | `Float f ->     `Float f
-  | `Infinity ->    `Float (1.0 /. 0.0)
-  | `Neginfinity -> `Float (-1.0 /. 0.0)
-  | `Nan ->         `Float (0.0 /. 0.0)
+    let number = function
+    | `Float f ->     `Float f
+    | `Infinity ->    `Float (1.0 /. 0.0)
+    | `Neginfinity -> `Float (-1.0 /. 0.0)
+    | `Nan ->         `Float (0.0 /. 0.0)
 
+  end
+
+  module Lexxer = Compliant_lexxer.Make(Compliance)
+  module Parser = Parser.Make(Compliance)
+  include Json_string_file.Make (Lexxer) (Parser)
+
+  include Json_writer_string.Make(Compliance)
+  include Json_writer_file.Make(Compliance)
 end
 
-module Lexxer = Compliant_lexxer.Make(Compliance)
-module Parser = Parser.Make(Compliance)
-include Json_string_file.Make (Lexxer) (Parser)
+module Safe = struct
+  module Compliance = struct
+    type json = Json.Extended.json
+    type t = json
 
-include Json_writer_string.Make(Compliance)
-include Json_writer_file.Make(Compliance)
+    open Tokens
+
+    let lex_number token = token
+    let lex_integer token = token
+    let lex_largeint token = token
+
+    let lex_variant _ = true
+    let lex_tuple _ = true
+
+    let number_to_string f =
+      match classify_float f with
+      | FP_normal | FP_subnormal | FP_zero ->
+        Json_float.string_of_float_fast_int f
+      | FP_infinite ->
+        if f < 0. then "-Infinity" else "Infinity"
+      | FP_nan ->
+        "NaN"
+
+    let integer i = `Int i
+    let null = `Null
+    let string s = `String s
+    let bool b = `Bool b
+    let assoc a = `Assoc a
+    let list l = `List l
+    let tuple l = `Tuple l
+    let variant k v = `Variant (k, v)
+
+    let number = function
+    | `Float f ->     `Float f
+    | `Infinity ->    `Float (1.0 /. 0.0)
+    | `Neginfinity -> `Float (-1.0 /. 0.0)
+    | `Nan ->         `Float (0.0 /. 0.0)
+
+  end
+
+  module Lexxer = Compliant_lexxer.Make(Compliance)
+  module Parser = Parser.Make(Compliance)
+  include Json_string_file.Make (Lexxer) (Parser)
+
+  include Json_writer_string.Make(Compliance)
+  include Json_writer_file.Make(Compliance)
+end
