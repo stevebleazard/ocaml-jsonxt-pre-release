a7aabce17b4eb19dce66d92c4ea1c6186cce38c5
stevebleazard <stevebleazard@googlemail.com>
2018-04-01 15:54:54 +0100

    continuation of compliance test framework

--- test_src/compliance_tester.ml
+++ test_src/compliance_tester.ml
@@ -1,16 +1,39 @@
 open Printf
 
+let help_error err = 
+  match err with
+  | Some err -> printf "ERROR: %s\n\n" err
+  | None -> ()
+
 let help err =
-  begin
-    match err with
-    | Some err -> printf "ERROR: %s\n\n" err
-    | None -> ()
-  end;
+  help_error err;
   printf "compliance_tester [help|suite|internal]\n";
   exit 0
 
+let help_internal err =
+  help_error err;
+  printf "
+   compliance_tester internal [help|run <file>]
+
+   run internal compliance testing as defined in <file>. This has the format
+     <level> [pass|fail] <filename>
+
+   where
+     <level>
+       one of strict, basic, extended, yjbasic and yjsafe. These correspond
+       to the various compliance levels (yjbasic maps to Yojson.Basic etc)
+     [pass|fail]
+       indicates the expected outcome
+     <filename>
+       the file containing the test, it is assumed to be in the current directory.
+       
+   Note that any number of spaces can seperate the fields.\n"
+
+let run_internal_tests idx =
+  help_internal None
+  
 let run_test_suite _idx = ()
-let run_internal_tests _idx = ()
+
 
 let () =
   if Array.length Sys.argv < 2 then help (Some "expected sub-test to run");
