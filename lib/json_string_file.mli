a3e2754ba129b5bd464908ca72928479ef9e3835
stevebleazard <stevebleazard@googlemail.com>
2018-04-07 15:32:16 +0100

    doc

--- lib/json_string_file.mli
+++ lib/json_string_file.mli
@@ -1,10 +1,23 @@
 module type Json_string_file = sig
   type json
 
+  (** [json_of_string string] convers [string] to a [json] value returing an error
+      if the string has syntax, grammer or compliance error *)
   val json_of_string : string -> (json, string) result
+
+  (** [json_of_string_exn string] convers [string] to a [json] value raising a
+      Failure exception if the string has syntax, grammer or compliance error *)
   val json_of_string_exn : string -> json
+
+  (** [json_of_file file] convers [file] to a [json] value returing an error
+      if the file has syntax, grammer or compliance error. The file is closed on error *)
   val json_of_file : string -> (json, string) result
+
+  (** [json_of_file file] convers [file] to a [json] value raising a Failure exception
+      if the file has syntax, grammer or compliance error. The file is closed on error *)
   val json_of_file_exn : string -> json
+
+  (** [of_string] is an alias for json_of_string_exn *)
   val of_string : string -> json
 end
 
