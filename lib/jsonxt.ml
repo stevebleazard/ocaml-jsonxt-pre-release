e3cd9da7660b58d7066b43647a4f00d5bae39a62
stevebleazard <stevebleazard@googlemail.com>
2018-04-07 19:04:55 +0100

    removed testing exports

--- lib/jsonxt.ml
+++ lib/jsonxt.ml
@@ -3,5 +3,4 @@ module Extended = Extended
 module Yojson = Yojson
 module Basic = Basic
 module Strict = Strict
-module Json_float = Json_float (* CR remove once testing complete *)
 module Tools = Tools
