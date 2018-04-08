a6a6c2c0de42b54ef5b8a0a8779f9f4b12c97842
stevebleazard <stevebleazard@googlemail.com>
2018-04-08 17:06:32 +0100

    start of refactoring writer and writer_hum to a single shared function

--- lib/json_writer_file.ml
+++ lib/json_writer_file.ml
@@ -120,6 +120,57 @@ module Make (Compliance : Compliance.S) : Intf = struct
     in
     fmt "" json
 
+  let json_to_channel_fmt' oc json ~off ~eol ~incr = 
+    let add_char = output_char oc in
+    let add_string = output_string oc in
+    let add_quote_string s = add_char '"'; escape oc s; add_char '"' in
+    let add_int i = add_string (string_of_int i) in
+    let add_float f = add_string (Compliance.number_to_string f) in
+    let rec fmt off value =
+      match value with
+      | `Assoc o ->
+        let ldr = String.make off ' ' in
+        add_char '{'; add_string eol; json_assoc (off + incr) o;
+        add_string eol; add_string ldr; add_char '}'
+      | `List l ->
+        let ldr = String.make off ' ' in
+        add_char '['; add_string eol; json_list (off + incr) l;
+        add_char '\n'; add_string ldr; add_char ']'
+      | `Null -> add_string "null"
+      | `Bool b -> add_string (string_of_bool b)
+      | `Int i -> add_int i
+      | `Intlit s -> add_quote_string s
+      | `Float f -> add_float f
+      | `Floatlit s -> add_quote_string s
+      | `String s -> add_quote_string s
+      | `Stringlit s -> add_quote_string s
+      | `Tuple t ->
+        let ldr = String.make off ' ' in
+        add_char '('; add_string eol; json_list (off + incr) t;
+        add_char '\n'; add_string ldr; add_char ')'
+      | `Variant v ->
+        let ldr = String.make off ' ' in
+        add_char '<'; add_string eol;  variant (off + incr) v;
+        add_char '\n'; add_string ldr; add_char '>'
+    and json_assoc off o =
+      let ldr = String.make off ' ' in
+      let sep = ref ldr in
+      let newsep = "," ^ eol ^ ldr in
+      List.iter (fun v -> add_string !sep; sep := newsep; pair off v ) o
+    and pair off (k, v) = add_quote_string k; add_string ": "; fmt off v
+    and json_list ldr l =
+      let ldr = String.make off ' ' in
+      let sep = ref ldr in
+      let newsep = "," ^ eol ^ ldr in
+      List.iter (fun v -> add_string !sep; sep := newsep; fmt off v ) l
+    and variant off (k, j) =
+      add_quote_string k;
+      match j with
+      | Some j -> add_string ": "; fmt (off + incr) j
+      | None -> ()
+    in
+    fmt 0 json
+
   let json_to_channel oc json =
     try Ok (json_to_channel' oc json) with
     | Failure err -> Error err
