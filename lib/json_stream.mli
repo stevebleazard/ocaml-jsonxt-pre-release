722fb4799f0737f63cf13445432d4a29e4608f59
stevebleazard <stevebleazard@googlemail.com>
2018-04-15 18:41:08 +0100

    new json_stream module for streaming parser

---
+++ lib/json_stream.mli
@@ -0,0 +1,63 @@
+open Tokens
+type json =
+    [
+    | `Null
+    | `Bool of bool
+    | `Int of int
+    | `Intlit of string
+    | `Float of float
+    | `Floatlit of string
+    | `String of string
+    | `Stringlit of string
+    | `As
+    | `Ae
+    | `Os
+    | `Oe
+    | `Ts
+    | `Te
+    | `Vs
+    | `Ve
+    | `Name of string
+    | `Infinity
+    | `Neg_infinity
+    | `Nan
+    ]
+type t = json
+
+module Extended : sig
+  type nonrec json = json
+  type t = json
+end
+
+module Basic : sig
+  type json =
+      [
+      | `Null
+      | `Bool of bool
+      | `Int of int
+      | `Float of float
+      | `String of string
+      | `As
+      | `Ae
+      | `Os
+      | `Oe
+      | `Name of string
+      ]
+  type t = json
+end
+
+module Strict : sig
+  type json =
+      [
+      | `Null
+      | `Bool of bool
+      | `Float of float
+      | `String of string
+      | `As
+      | `Ae
+      | `Os
+      | `Oe
+      | `Name of string
+      ]
+  type t = json
+end
