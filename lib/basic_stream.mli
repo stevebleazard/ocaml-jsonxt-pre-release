af29f25684c12d60a3a787fba3cd1b8344dbc263
stevebleazard <stevebleazard@googlemail.com>
2020-12-30 08:19:58 +0000

    fixed stream writer type checking

--- lib/basic_stream.mli
+++ lib/basic_stream.mli
@@ -1,4 +1,4 @@
 type json_stream = Json_stream.Basic.json
 
 include (Reader_stream.Reader_stream with type json_stream := json_stream)
-include (Writer_stream.Intf)
+include (Writer_stream_intf.Intf with type json_stream := json_stream)
