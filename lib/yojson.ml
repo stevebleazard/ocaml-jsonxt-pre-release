b90ec46d54e047d67ae9f08bfa3df554275c0776
stevebleazard <stevebleazard@googlemail.com>
2021-01-10 13:41:53 +0000

    added raw part 1 to yojson

--- lib/yojson.ml
+++ lib/yojson.ml
@@ -5,6 +5,10 @@ module Basic = struct
 
     open Tokens
 
+    let lex_string = function
+      | Tokens.STRING s -> Tokens.STRING (Lexxer_utils.unescape_string s)
+      | token -> token
+
     let lex_number token = token
     let lex_integer token = token
     let lex_largeint _ = COMPLIANCE_ERROR "Integer out of bounds"
@@ -36,6 +40,7 @@ module Basic = struct
     | `Infinity ->    `Float (1.0 /. 0.0)
     | `Neginfinity -> `Float (-1.0 /. 0.0)
     | `Nan ->         `Float (0.0 /. 0.0)
+    | `Floatlit _ ->  raise (Failure "floatlit not supported in basic mode")
 
     module Stream = struct
       let number = number
@@ -71,6 +76,10 @@ module Safe = struct
     type json = Json.Extended.json
     type json_stream = Json_stream.Extended.json
 
+    let lex_string = function
+      | Tokens.STRING s -> Tokens.STRING (Lexxer_utils.unescape_string s)
+      | token -> token
+
     let lex_number token = token
     let lex_integer token = token
     let lex_largeint token = token
@@ -102,6 +111,78 @@ module Safe = struct
     | `Infinity ->    `Float (1.0 /. 0.0)
     | `Neginfinity -> `Float (-1.0 /. 0.0)
     | `Nan ->         `Float (0.0 /. 0.0)
+    | `Floatlit _ ->  raise (Failure "floatlit not supported in basic mode")
+
+    module Stream = struct
+      let number = number
+      let largeint = largeint
+      let integer = integer
+      let null = null
+      let string = string
+      let bool = bool
+
+      let array_start () = `As
+      let array_end () = `Ae
+      let object_start () = `Os
+      let object_end () = `Oe
+      let tuple_start () = `Ts
+      let tuple_end () = `Te
+      let variant_start () = `Vs
+      let variant_end () = `Ve
+      let name s = `Name s
+    end
+  end
+
+  module Lexxer = Compliant_lexxer.Make(Compliance)
+  module Parser = Parser.Make(Compliance)
+  include Reader_string_file.Make (Lexxer) (Parser)
+  type t = json
+
+  include Writer_string.Make(Compliance)
+  include Writer_file.Make(Compliance)
+end
+
+module Raw = struct
+  module Compliance = struct
+    type json = Json.Extended.json
+    type json_stream = Json_stream.Extended.json
+
+    let lex_string = function
+      | Tokens.STRING s -> Tokens.STRING ("\"" ^ s ^ "\"")
+      | token -> token
+
+    let lex_number token = token
+    let lex_integer token = token
+    let lex_largeint token = token
+
+    let lex_variant _ = true
+    let lex_tuple _ = true
+
+    let number_to_string f =
+      match classify_float f with
+      | FP_normal | FP_subnormal | FP_zero ->
+        Json_float.string_of_float_json f
+      | FP_infinite ->
+        if f < 0. then "-Infinity" else "Infinity"
+      | FP_nan ->
+        "NaN"
+
+    let largeint s = `Intlit s
+    let integer i = `Int i
+    let null = `Null
+    let string s = `Stringlit s
+    let bool b = `Bool b
+    let assoc a = `Assoc a
+    let list l = `List l
+    let tuple l = `Tuple l
+    let variant k v = `Variant (k, v)
+
+    let number = function
+    | `Float f ->     `Floatlit (string_of_float f)
+    | `Infinity ->    `Floatlit "Infinity"
+    | `Neginfinity -> `Floatlit "-Infinity"
+    | `Nan ->         `Floatlit "NaN"
+    | `Floatlit f ->  `Floatlit f
 
     module Stream = struct
       let number = number
--- lib/yojson.mli
+++ lib/yojson.mli
@@ -21,3 +21,15 @@ module Safe : sig
     type json = Json.Extended.json
     and type json_stream = Json_stream.Extended.json
 end
+
+module Raw : sig
+  type json = Json.Extended.json
+  type t = json
+
+  include (Reader_string_file.Reader_string_file with type json := json)
+  include (Writer_intf.Intf with type json := Json.Extended.json)
+
+  module Compliance : Compliance.S with
+    type json = Json.Extended.json
+    and type json_stream = Json_stream.Extended.json
+end
