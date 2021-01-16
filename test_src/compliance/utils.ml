f16c21f5a181a030807e9d24d5e483b0a945a055
stevebleazard <stevebleazard@googlemail.com>
2021-01-16 19:44:52 +0000

    prep for monad testing

--- test_src/compliance/utils.ml
+++ test_src/compliance/utils.ml
@@ -1,3 +1,43 @@
+module IO : sig
+  type 'a t
+
+  val return : 'a -> 'a t
+  val (>>=)  : 'a t -> ('a -> 'b t) -> 'b t
+  val result : 'a t -> 'a
+end = struct
+  type 'a t = 'a
+
+  let return v = v
+  let (>>=) v f = f v
+  let result t = t
+end
+
+module StringIO = struct
+  type t = {
+    buf : Buffer.t
+  ; off : int ref
+  }
+
+  let create s =
+    let iobuf = { buf = Buffer.create 32; off = ref 0 } in
+    Buffer.add_string iobuf.buf s;
+    iobuf
+
+  let read t buf len =
+    let buflen = Buffer.length t.buf in
+    let noff = !(t.off) + len in
+    let noff = if noff > buflen then buflen else noff in
+    let clen = noff - !(t.off) in
+    if clen <= 0 then begin Buffer.reset t.buf; t.off := 0; 0 end
+    else begin
+      Buffer.blit t.buf !(t.off) buf 0 clen;
+      t.off := noff;
+      clen
+    end
+
+  let write t s = Buffer.add_string t.buf s
+end
+
 (* Determine the size of an integer, handles 31bit, 63bit and Jsoo using 32bit ints *)
 let int_bits =
   let rec log2 n = if n <= 1 then 0 else 1 + log2(n asr 1) in
