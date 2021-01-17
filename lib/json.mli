340d3ac5df5b0c4db9705021bb0f49af850328ce
stevebleazard <stevebleazard@googlemail.com>
2021-01-17 09:58:44 +0000

    documented json types

--- lib/json.mli
+++ lib/json.mli
@@ -1,5 +1,19 @@
 (** Json types for the various compliance levels *)
 
+(** The following is a complete list of the polymorphic variants supported by Jsonxt
+    - `Null: JSON null
+    - `Bool of bool: JSON boolean
+    - `Int of int: JSON number without decimal point or exponent.
+    - `Intlit of string: JSON number without decimal point or exponent, preserved as a string.
+    - `Float of float: JSON number, inf, -inf, Infinity, -Infinity, nan, -nan, NaN or -NaN
+    - `Floatlit of string: JSON number, inf, -inf, Infinity, -Infinity, nan, -nan, NaN or -NaN, preserved as a string.
+    - `String of string: JSON string. Bytes in the range 128-255 are preserved when reading and writing.
+    - `Stringlit of string: JSON string literal including the double quotes.
+    - `Assoc of (string * json) list: JSON object.
+    - `List of json list: JSON array.
+    - `Tuple of json list: Tuple (non-standard extension of JSON). Syntax: ("abc", 123).
+    - `Variant of (string * json option): Variant (non-standard extension of JSON). Syntax: <"Foo"> or <"Bar":123>.
+*)
 type json =
     [
     | `Null
@@ -30,6 +44,15 @@ module Basic : sig
   (** [Basic] supports standard Json types that are supported by the
       JSON standard but also supports integers rather than just floats *)
 
+  (** The following polymorphic variants supported
+      - `Null: JSON null
+      - `Bool of bool: JSON boolean
+      - `Int of int: JSON number without decimal point or exponent.
+      - `Float of float: JSON number, inf, -inf, Infinity, -Infinity, nan, -nan, NaN or -NaN
+      - `String of string: JSON string. Bytes in the range 128-255 are preserved when reading and writing.
+      - `Assoc of (string * json) list: JSON object.
+      - `List of json list: JSON array.
+  *)
   type json =
       [
       | `Null
@@ -47,6 +70,14 @@ module Strict : sig
   (** [Strict] supports only types that are supported by the JSON standard.
       Integers are not supported *)
 
+  (** The following polymorphic variants supported
+      - `Null: JSON null
+      - `Bool of bool: JSON boolean
+      - `Float of float: JSON number, inf, -inf, Infinity, -Infinity, nan, -nan, NaN or -NaN
+      - `String of string: JSON string. Bytes in the range 128-255 are preserved when reading and writing.
+      - `Assoc of (string * json) list: JSON object.
+      - `List of json list: JSON array.
+  *)
   type json =
       [
       | `Null
--- lib/json_stream.mli
+++ lib/json_stream.mli
@@ -1,5 +1,27 @@
 (** Json stream types for the various compliance levels *)
 
+(** The following polymorphic variants are supported by Jsonxt
+    - `Null: JSON null
+    - `Bool of bool: JSON boolean
+    - `Int of int: JSON number without decimal point or exponent
+    - `Intlit of string: JSON number without decimal point or exponent preserved as a string
+    - `Float of float: JSON number
+    - `Floatlit of string: JSON number preserved as a string
+    - `String of string: JSON string with characters in the range 128-255 preserved
+    - `Stringlit of string: JSON string including the double quotes
+    - `As - Array start marker
+    - `Ae - Array end marker
+    - `Os - Object start marker
+    - `Oe - Object end marker
+    - `Ts - Tuple start marker
+    - `Te - Tuple end marger
+    - `Vs - Variant start marger
+    - `Ve - Variant end marger
+    - `Name of string - JSON object key as a JSON string
+    - `Infinity - Infinity
+    - `Neg_infinity - -Infinity
+    - `Nan - NaN
+ *)
 type json =
     [
     | `Null
@@ -37,6 +59,18 @@ module Basic : sig
   (** [Basic] supports standard Json types that are supported by the
       JSON standard but also supports integers rather than just floats *)
 
+  (** The following polymorphic variants are supported
+    - `Null: JSON null
+    - `Bool of bool: JSON boolean
+    - `Int of int: JSON number without decimal point or exponent
+    - `Float of float: JSON number
+    - `String of string: JSON string with characters in the range 128-255 preserved
+    - `As - Array start marker
+    - `Ae - Array end marker
+    - `Os - Object start marker
+    - `Oe - Object end marker
+    - `Name of string - JSON object key as a JSON string
+  *)
   type json =
       [
       | `Null
@@ -57,6 +91,17 @@ module Strict : sig
   (** [Strict] supports only types that are supported by the JSON standard.
       Integers are not supported *)
 
+  (** The following polymorphic variants are supported
+    - `Null: JSON null
+    - `Bool of bool: JSON boolean
+    - `Float of float: JSON number
+    - `String of string: JSON string with characters in the range 128-255 preserved
+    - `As - Array start marker
+    - `Ae - Array end marker
+    - `Os - Object start marker
+    - `Oe - Object end marker
+    - `Name of string - JSON object key as a JSON string
+  *)
   type json =
       [
       | `Null
