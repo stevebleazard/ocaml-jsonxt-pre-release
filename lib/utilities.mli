a37a894dbc87537b28bb766fd3132b84c699c685
stevebleazard <stevebleazard@googlemail.com>
2021-01-21 08:11:59 +0000

    documentation update

--- lib/utilities.mli
+++ lib/utilities.mli
@@ -4,10 +4,12 @@
 *)
 
 (** [to_basic json] converts [json] into the [Basic.json] type
-    transforming variants that do not comply to that type *)
+    transforming variants that do not comply to that type.
+    All json types can be converted *)
 val to_basic : 'a Json_internal.constrained -> Basic.json
 
 (** [to_basic json] converts [json] into the [Strict.json] type
     transforming variants that do not comply to that type.
-    In particular ints are converted to floats *)
+    In particular ints are converted to floats. All json types can
+    be converted *)
 val to_strict : 'a Json_internal.constrained -> Strict.json
