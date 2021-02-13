b8660797a576cdb42d2c6fb3da4aa9426a472271
stevebleazard <stevebleazard@googlemail.com>
2021-02-13 15:44:32 +0000

    better writing of some floats

--- lib/json_float.ml
+++ lib/json_float.ml
@@ -1,5 +1,4 @@
 external format_float : string -> float -> string = "caml_format_float"
-external format_int : string -> int -> string = "caml_format_int"
 
 (* Determine the size of an integer, handles 30bit, 62bit and Jsoo using 32bit ints *)
 let max_json_int =
@@ -12,7 +11,12 @@ let max_json_int_as_float = float_of_int max_json_int
 let string_of_float_json f =
   let is_int = (float_of_int (int_of_float f)) = f in
   if is_int && Float.abs f <= max_json_int_as_float then begin (* IEEE max int in a float when in 64bit int mode*)
-    let int_value = int_of_float f in (format_int "%lld" int_value) ^ ".0"
+    let int_value = int_of_float f in (string_of_int int_value) ^ ".0"
   end
-  else
-    format_float "%.17g" f
+  else begin
+    (* %.17g often creates overly long output, attempt to convert at lower precession first *)
+    let s = format_float "%.16g" f in
+    let s = if float_of_string s = f then s else format_float "%.17g" f in
+    if not (String.contains s '.') then s ^ ".0"
+    else s
+  end
